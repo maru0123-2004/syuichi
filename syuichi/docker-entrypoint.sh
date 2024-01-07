4b3ac0f0c8d176113826e9e811b49459be375af1
Maruo.S <raspi-maru2004@outlook.jp>
2024-01-08 01:59:10 +0900

    setup dood

--- syuichi/docker-entrypoint.sh
+++ syuichi/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 USER_ID=${LOCAL_USER_ID:-1000}
-usermod -u $USER_ID nonroot
+groupadd -f -g $(stat -c %g /var/run/docker.sock) docker
+usermod -u $USER_ID -aG docker nonroot
 chown -R nonroot ./
 
 exec /usr/local/bin/pysu nonroot "$@"
