6986dacfde783dc03437cc3912fb53503d848763
Maruo.S <raspi-maru2004@outlook.jp>
2024-01-13 17:24:21 +0900

    fix spa

--- syuichi/templates/assets/Home-b942c7d9.js
+++
@@ -1,1 +0,0 @@
-const r=(t,e)=>{const c=t.__vccOpts||t;for(const[n,o]of e)c[n]=o;return c},_={};function s(t,e){return" Home "}const a=r(_,[["render",s]]);export{a as default};
--- syuichi/urls.py
+++ syuichi/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import index
@@ -24,5 +24,6 @@ from .dockerctrl.urls import router as dockerctrl_router
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(dockerctrl_router.urls)),
-    path('', index, name='index'),
-] + static(settings.STATIC_URL)
+    *static(settings.STATIC_URL),
+    re_path('.*', index, name='index'),
+]
